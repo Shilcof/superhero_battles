dd5728300ab56c9ee59ede05155deb8835f23235
Shilcof <robert.shilcof@gmail.com>
2020-11-06 11:28:22 +0000

    Initial commit

---
+++ lib/superhero_battles/cli.rb
@@ -0,0 +1,17 @@
+class CLI
+
+    def call
+        # Welcomes the user
+        puts "Welcome to Superhero Battles"
+        # Asks for optional name?
+        # Menu of what to do
+        menu
+    end
+
+    def menu
+        puts "Please select which option you would like:"
+
+
+    end
+
+end
